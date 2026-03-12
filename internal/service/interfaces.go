f207639c871aa9cfc03e6b3affe08b68cf7f0a0d
Rustam Aliev <donkodonko583@gmail.com>
2026-03-12 20:48:15 +0500

    refactor: add logaut_service

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -1,71 +1,10 @@
 package service
 
-import (
-	"context"
-	"time"
-
-	"github.com/redis/go-redis/v9"
-)
+import "context"
 
 type LogoutService interface {
 	Logout(ctx context.Context, tokenJTI string) error
 	IsTokenBlacklisted(ctx context.Context, tokenJTI string) (bool, error)
 	BlacklistRefreshToken(ctx context.Context, tokenJTI string) error
 	IsRefreshTokenBlacklisted(ctx context.Context, tokenJTI string) (bool, error)
-}
-
-type LogoutServiceImpl struct {
-	redisClient     *redis.Client
-	accessTokenTTL  time.Duration
-	refreshTokenTTL time.Duration
-}
-
-func NewLogoutService(redisClient *redis.Client, accessTokenTTL, refreshTokenTTL time.Duration) LogoutService {
-	return &LogoutServiceImpl{
-		redisClient:     redisClient,
-		accessTokenTTL:  accessTokenTTL,
-		refreshTokenTTL: refreshTokenTTL,
-	}
-}
-
-func (s *LogoutServiceImpl) Logout(ctx context.Context, tokenJTI string) error {
-	key := "blacklist:" + tokenJTI
-	err := s.redisClient.Set(ctx, key, "revoked", s.accessTokenTTL).Err()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *LogoutServiceImpl) IsTokenBlacklisted(ctx context.Context, tokenJTI string) (bool, error) {
-	key := "blacklist:" + tokenJTI
-	val, err := s.redisClient.Get(ctx, key).Result()
-	if err == redis.Nil {
-		return false, nil
-	}
-	if err != nil {
-		return false, err
-	}
-	return val == "revoked", nil
-}
-
-func (s *LogoutServiceImpl) BlacklistRefreshToken(ctx context.Context, tokenJTI string) error {
-	key := "refresh_blacklist:" + tokenJTI
-	err := s.redisClient.Set(ctx, key, "revoked", s.refreshTokenTTL).Err()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *LogoutServiceImpl) IsRefreshTokenBlacklisted(ctx context.Context, tokenJTI string) (bool, error) {
-	key := "refresh_blacklist:" + tokenJTI
-	val, err := s.redisClient.Get(ctx, key).Result()
-	if err == redis.Nil {
-		return false, nil
-	}
-	if err != nil {
-		return false, err
-	}
-	return val == "revoked", nil
 }
